f0792e643cdfe8cb18d8691bd08920d580efe88c
JanselLopez <21jansel@gmail.com>
2025-09-15 23:50:45 -0400

    chore: exclude summary from get dispute

--- utils/get_resume_report.py
+++ utils/get_resume_report.py
@@ -2,7 +2,12 @@ from services.bd import vector_store
 
 def get_resume_report(user_id: str):
     results = vector_store.get(
-        where={"user_id": user_id},  # filter by user_id tag/metadata
+        where={
+            "$and": [
+                {"user_id": user_id},
+                {"source": {"$ne": "CreditSummary"}}
+            ]
+        },  # filter by user_id tag/metadata
         limit=None  # or a very high number if None is not supported
     )
     return results['documents']
